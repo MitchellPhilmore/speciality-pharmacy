8d76d7e9a78e53d4349f6488aedef1030308a378
Mitch Philmore <Mitch@Mitchs-MacBook-Air.local>
2020-07-15 19:37:01 -0400

    Updates

---
+++ precache-manifest.e376a2868dfb9040128c6ac3a7d03d07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93a9129a18f7354f3227",
+    "url": "/speciality-pharmacy.github.io/static/css/main.a2984011.chunk.css"
+  },
+  {
+    "revision": "93a9129a18f7354f3227",
+    "url": "/speciality-pharmacy.github.io/static/js/main.93a9129a.chunk.js"
+  },
+  {
+    "revision": "64798fb162a352fa1f23",
+    "url": "/speciality-pharmacy.github.io/static/js/runtime~main.64798fb1.js"
+  },
+  {
+    "revision": "dd0dc3597bfe349fd85c",
+    "url": "/speciality-pharmacy.github.io/static/js/2.dd0dc359.chunk.js"
+  },
+  {
+    "revision": "40eaa426c1e56d5b9a07c5090c4d437a",
+    "url": "/speciality-pharmacy.github.io/static/media/pharmacy-logo.40eaa426.png"
+  },
+  {
+    "revision": "ebf776478cb667e5d9fe23639bcd41fa",
+    "url": "/speciality-pharmacy.github.io/index.html"
+  }
+];
